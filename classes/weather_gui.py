f226123da97ec416ae08126226930c31384ea897
aintyour.keii <klynepascual.edu@gmail.com>
2025-06-26 14:59:52 +0800

    WeatherGui: __init__ function completed

--- classes/weather_gui.py
+++ classes/weather_gui.py
@@ -1,3 +1,12 @@
+import tkinter as tk
+from tkinter import ttk, messagebox
+from classes.weather_app import WeatherApp
+
 class WeatherGui:
     def __init__(self):
-        pass
+        self.weather_app = WeatherApp()
+        
+        self.root = tk.Tk()
+        self.root.title("Weather App")
+        self.root.geometry("500x500")
+        self.root.resizable(False, False)

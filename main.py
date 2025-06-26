ac93316ee54f3b2c41e54166514df6f6ebfefc08
aintyour.keii <klynepascual.edu@gmail.com>
2025-06-26 14:00:39 +0800

    Pseudo Code Created for Core Weather App

---
+++ main.py
@@ -0,0 +1,36 @@
+# Create Reference to Weather API (OpenWeatherMap)
+# Create a variable to store Base Url
+
+# ----------------------------------------------------
+# Create a function to get the weather of a city
+# Complete the full url using the Base Url and parameters such as city name, api, units
+# Send an HTTP Request to the completed url
+# Parse the Json Response
+
+# Check errors from the respose
+# If there is an error then display the error message
+# Otherwise Extract the neccessary information:
+# -> City Name
+# -> Country
+# -> Temperature
+# -> Weather Description
+# -> Humidity
+# -> Wind Speed
+
+# Return the extracted Value 
+# ----------------------------------------------------
+
+# ----------------------------------------------------
+# Create the main() function
+# Prompt the user for a city name
+# Call the get_weather function with the city name as the parameter
+# Check if the funciton returns anything, if it does then:
+# Display:
+# -> City, Country
+# -> Weather Description
+# -> Temperature
+# -> Humidity
+# -> Wind Speed
+# ----------------------------------------------------
+
+# Start main()
